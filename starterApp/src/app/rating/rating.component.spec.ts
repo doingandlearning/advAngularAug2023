e377ab2adadfbb198256c7bb253d66e70d19c499
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-08-07 14:21:21 +0100

    Adds the rating tests

--- starterApp/src/app/rating/rating.component.spec.ts
+++ starterApp/src/app/rating/rating.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RatingComponent } from './rating.component';
+import { By } from '@angular/platform-browser';
 
 describe('RatingComponent', () => {
   let component: RatingComponent;
@@ -8,7 +9,7 @@ describe('RatingComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RatingComponent]
+      declarations: [RatingComponent],
     });
     fixture = TestBed.createComponent(RatingComponent);
     component = fixture.componentInstance;
@@ -18,4 +19,31 @@ describe('RatingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit the correct rating when updated', () => {
+    // Arrange
+    // Act
+    spyOn(component.ratingChange, 'emit');
+    component.updateRating(5);
+
+    // Assert
+    expect(component.ratingChange.emit).toHaveBeenCalledTimes(1);
+    expect(component.ratingChange.emit).toHaveBeenCalledWith(5);
+  });
+
+  it('should not go below 1', () => {});
+
+  it('should not go above the max', () => {});
+
+  it('should increase when increase button is clicked', () => {
+    const button = fixture.debugElement.query(By.css('#addButton'));
+    component.currentRating = 1;
+
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.currentRating).toBe(2);
+  });
+
+  it('should decrease when the decrease button is clicked', () => {});
 });
